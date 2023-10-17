41150f9cf4d7c0d81dc7c3a57ef97a5a09270789
shutter <6331335921@student.chula.ac.th>
2023-10-17 23:48:07 +0700

    API authorization & Data Posting

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -1,8 +1,13 @@
 import Image from "next/image";
 import styles from "./TopMenu.module.css";
 import TopMenuItem from "./TopMenuItem";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { Link } from "@mui/material";
 
-export default function Topmenu() {
+export default async function Topmenu() {
+  const session = await getServerSession(authOptions);
+
   return (
     <div className={styles.menucontainer}>
       <Image
@@ -15,6 +20,19 @@ export default function Topmenu() {
       />
       <TopMenuItem title="Reservations" pageRef="/reservations" />
       <TopMenuItem title="About" pageRef="/about" />
+      {session ? (
+        <Link href="/api/auth/signout">
+          <div className="flex items-center absolute right-0 h-full px-3 text-cyan-600 text-sm">
+            Sign-Out of {session.user?.name}
+          </div>
+        </Link>
+      ) : (
+        <Link href="/api/auth/signin">
+          <div className="flex items-center absolute right-0 h-full px-3 text-cyan-600 text-sm">
+            Sign-In
+          </div>
+        </Link>
+      )}
     </div>
   );
 }
